4de4b6fd12b45b265245f5430ac563ae0cf52298
Rob Reiss <robreiss@hotmail.com>
2022-11-22 19:21:06 -0800

    identify block

--- IceBase/identify_block.ts
+++ IceBase/identify_block.ts
@@ -20,6 +20,56 @@
 import assert from "assert";
 
 function identifyBlock(values: number[]): string | null {
+    values = values.sort((a, b) => { return a - b })
+    let a: [string, number[]][] = [
+        ['I', [1, 2, 3, 4]],
+        ['I', [1, 5, 9, 13]],
+        ['J', [2, 6, 9, 10]],
+        ['J', [1, 5, 6, 7]],
+        ['J', [1, 2, 5, 9]],
+        ['J', [1, 2, 3, 7]],
+        ['L', [1, 5, 9, 10]],
+        ['L', [1, 2, 3, 5]],
+        ['L', [1, 2, 6, 10]],
+        ['L', [3, 5, 6, 10]],
+        ['O', [1, 2, 5, 6]],
+        ['S', [2, 3, 5, 6]],
+        ['S', [1, 5, 6, 10]],
+        ['T', [1, 2, 3, 6]],
+        ['T', [2, 5, 6, 10]],
+        ['T', [1, 5, 6, 9]],
+        ['T', [2, 5, 6, 7]],
+        ['Z', [1, 2, 6, 7]],
+        ['Z', [2, 5, 6, 9]],
+    ]
+
+    for (let c of a) {
+        // console.log(c[1].toString())
+        for (let y = 0; y < 4; y++) {
+            for (let i = 0; i < 4; i++) {
+                let x: number[] = []
+                let addnum = y * 4 + i
+                for (let l = 0; l < 4; l++) {
+                    let v = c[1][l] + addnum
+                    // console.log(addnum)
+                    // console.log(Math.floor((c[1][l] + 1) / 4), Math.floor((v + 1) / 4))
+                    // console.log(c[1][l]-1, Math.floor(((c[1][l])-1) / 4))
+                    if (Math.floor(((c[1][l] + y * 4) - 1) / 4) !== Math.floor((v - 1) / 4)) {
+                        v = 0
+                    } else if (v > 16) {
+                        v = 0
+                    }
+                    x.push(v)
+                }
+                // console.log(c[1].toString(), x.toString(), values.toString())
+                if (x.toString() == values.toString()) {
+                    // console.log('match', c[0])
+                    return c[0].toString()
+                }
+            }
+        }
+    }
+    return null
     /*
     grid(4x4):
     +--+--+--+--+
@@ -41,11 +91,12 @@ function identifyBlock(values: number[]): string | null {
      *
 
     */
-    return '';
 }
 
 console.log('Example:');
-console.log(identifyBlock([10, 13, 14, 15]));
+// console.log(identifyBlock([10, 13, 14, 15]));
+console.log(identifyBlock([2, 3, 7, 11]))
+// console.log(identifyBlock([1, 5, 9, 6]))
 
 // These "asserts" are used for self-checking
 assert.equal(identifyBlock([10, 13, 14, 15]), 'T');
